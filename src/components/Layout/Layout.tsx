8ac4510d6997d0992a88b8ba729989d70c20dfb8
Egor K <murwoofmur@gmail.com>
2025-01-13 18:05:11 +0700

    chore: add Material UI as a dependency

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,25 @@
 import {navigationList} from "../../routes/navigationList.ts";
+import {Box, Button, Stack} from "@mui/material";
 import {Link, Outlet} from "react-router-dom";
 
 export function Layout() {
 
     return (
-        <div className="container">
-            <div className='content'>
-                {navigationList.map(item => {
-                    return (<Link key={item.path} to={item.path}>{item.title}</Link>)
-                })}
-                <Outlet/>
-            </div>
-        </div>
+        <Box>
+            <Stack spacing={1} direction='row'>
+                {navigationList.map((item) => (
+                    <Button
+                        key={item.path}
+                        component={Link}
+                        variant='outlined'
+                        color='primary'
+                        to={item.path}
+                    >
+                        {item.title}
+                    </Button>
+                ))}
+            </Stack>
+            <Outlet/>
+        </Box>
     )
 }
